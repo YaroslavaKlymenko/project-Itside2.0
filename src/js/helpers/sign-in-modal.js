1a2b5b90b1856d78106fbc66d8fa60845ad6ccae
YaroslavaKlymenko <klymenko0105@gmail.com>
2023-05-17 12:56:44 +0200

    user name in sign in

--- src/js/helpers/sign-in-modal.js
+++ src/js/helpers/sign-in-modal.js
@@ -1,5 +1,4 @@
 import { app } from "./firebase-application.js"
-import { getDatabase, ref, set, onValue } from 'firebase/database';
 import {
     getAuth,
     signInWithEmailAndPassword,
@@ -7,9 +6,8 @@ import {
     signOut,
   } from 'firebase/auth';
 import Notiflix from 'notiflix';
-// tedddf
+
 const backdrop = document.querySelector('.authorization__bacdrop');
-const db = getDatabase(app);
 const auth = getAuth(app);
 const logInBtn = document.querySelector('.log-in-btn');
 const userBarBtnText = document.querySelector('.user-bar-btn__text');
@@ -27,18 +25,18 @@ async function handelSignInUserAccount(e) {
     const userEmail = email.value;
     const userPassword = password.value;
   
-    if ( await signInUserAccount(auth, userEmail, userPassword)) {
-        e.target.reset();
+    if (await signInUserAccount(auth, userEmail, userPassword)) {
+      e.target.reset();
     }
   }
 
   async function signInUserAccount(auth, userEmail, userPassword) {
     try {
-    signInWithEmailAndPassword(auth, userEmail, userPassword)
-   .then((obj) => console.log(obj))
-        backdrop.style.display = 'none';
-        backdrop.classList.add('is-hidden');
-        logInBtn.classList.add('visually-hidden');
+      await signInWithEmailAndPassword(auth, userEmail, userPassword);
+
+      backdrop.style.display = 'none';
+      backdrop.classList.add('is-hidden');
+      logInBtn.classList.add('visually-hidden');
     }
       catch(error) {
         if (error.code === 'auth/wrong-password') {
@@ -53,29 +51,19 @@ async function handelSignInUserAccount(e) {
   function checkUserAuth() {
     onAuthStateChanged(auth, user => {
       if (user) {
-        const userNameRef = ref(db, 'users/' + user.uid);
-        console.log(userNameRef.name);
-        onValue(userNameRef, name => {
-          const currentUserName = name.exportVal();
-          userBarBtnText.innerHTML = currentUserName.username;
-          console.log(currentUserName)
-        });
+        userBarBtnText.innerHTML = user.displayName;
         userBar.classList.remove('visually-hidden');
         logInBtn.classList.add('visually-hidden');
         headerNav.classList.remove('is-hidden');
         headerNav.classList.remove('is-hidden');
-
       }
     });
   }
   checkUserAuth();
-  
-
 
   const signInForm = document.querySelector('#sign-in');
   signInForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log("text");
     handelSignInUserAccount(event);
 });
 
@@ -88,20 +76,10 @@ function handelLogOutUserAccount() {
         logInBtn.classList.remove('visually-hidden');
         backdrop.style.display = 'block';
         userBarBtnText.innerHTML = '';
-headerNav.classList.add('is-hidden');
-  
-        localStorage.removeItem('user');
-      })
+        headerNav.classList.add('is-hidden');
+        })
       .catch(error => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
+        console.log(error);
       });
   }
   
-  const userBarBtn = document.querySelector('.js-user-bar-btn');
-
-  userBarBtn.addEventListener('click', () => {
-    userBarBtn.classList.toggle('is-active');
-  });
-
-  export { checkUserAuth }
